977e28dac7f16256622447fa4ab48e8b8ff575eb
Rahul Vimal <155147272+rvking47@users.noreply.github.com>
2026-01-04 21:51:16 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:7002/api",
+    baseURL: "https://mern-reactflow-ai-app.onrender.com/api",
 });
+
 
 export default api;
